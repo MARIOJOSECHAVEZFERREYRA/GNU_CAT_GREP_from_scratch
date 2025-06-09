3a4ab659ac9f86c60438929a322373387704d43e
mario <mariojosechavezferreyra@gmail.com>
2025-06-09 21:25:45 +0300

    he rehecho s21 cat

--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,15 +1,21 @@
 #ifndef _S21_CAT_H_
 #define _S21_CAT_H_
 
+
+#define SUCCESS 0
+#define ERROR_INPUT -1
+#define ERROR_FILE_READ -2
+
 #include <stdio.h>
-#include <unistd.h>
-#include <getopt.h>
 
 typedef struct {
     int b, e, n, s, t, v;
 } Flags;
 
 
-void cat_file(const char *filename, Flags flags, int *line_num);
-
+int parseInput(int argc, char* argv[], Flags* flags);
+void markFlags(Flags* flags, int opt);
+void catFile(FILE* file, Flags* flags);
+void printNonPrinting(int c);
+void usage();
 #endif
