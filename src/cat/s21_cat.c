2fc9abce1de4daf880cb0292903a30233c824386
mario <mariojosechavezferreyra@gmail.com>
2025-06-11 00:17:52 +0300

    cat refactorizado

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -2,33 +2,28 @@
 #define _GNU_SOURCE
 
 #include "s21_cat.h"
-
-#include <ctype.h>
-#include <getopt.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-  int error_code = SUCCESS;
+int main(int argc, char *argv[]) {
   Flags flags = {0};
-  int fileArgumentIndex = ERROR_INPUT;
+  int line_num = 1;
+  int error_code = SUCCESS;
+  error_code = parseInput(argc, argv, &flags);
 
-  if (argc < 1 || (fileArgumentIndex = parseInput(argc,argv, &flags)) == -1 )
-    error_code = ERROR_INPUT;
-  else {
-    FILE* file = NULL;
-    if (argc == fileArgumentIndex) file = stdin;
-    while (error_code == SUCCESS && (fileArgumentIndex < argc || file == stdin)) {
-      if (file != stdin && (file = fopen(argv[fileArgumentIndex], "r")) == NULL) {
-        perror(argv[fileArgumentIndex]);
-        continue;
+  if (error_code != SUCCESS) {
+    printUsage(argv[0]);
+  } else {
+    argc -= optind;
+    argv += optind;
+    if (argc == 0) {
+      catFile(NULL, flags, &line_num);
+    } else {
+      for (int i = 0; i < argc; i++) {
+        catFile(argv[i], flags, &line_num);
       }
-      catFile(file, &flags);
-      fclose(file);
-      fileArgumentIndex++;
     }
   }
-  if (error_code == ERROR_INPUT) usage();
+
   return error_code;
 }
 
@@ -36,21 +31,22 @@ int parseInput(int argc, char* argv[], Flags* flags) {
   int error_code = SUCCESS;
   int opt;
   static struct option long_options[] = {
-      {"number-nonblank", no_argument, 0, 'b'},
-      {"number", no_argument, 0, 'n'},
-      {"squeeze-blank", no_argument, 0, 's'},
-      {"show-ends", no_argument, 0, 'E'},
-      {"show-nonprinting", no_argument, 0, 'v'},
-      {"show-tabs", no_argument, 0, 'T'},
-      {0, 0, 0, 0}};
+    {"number-nonblank", no_argument, 0, 'b'},
+    {"number", no_argument, 0, 'n'},
+    {"squeeze-blank", no_argument, 0, 's'},
+    {"show-ends", no_argument, 0, 'E'},
+    {"show-nonprinting", no_argument, 0, 'v'},
+    {"show-tabs", no_argument, 0, 'T'},
+    {0, 0, 0, 0}};
 
-  while ((opt = getopt_long(argc, argv, "benstvET", long_options, NULL)) !=
-         -1) {
+  while ((opt = getopt_long(argc, argv, "benstvET", long_options, NULL)) != -1) {
     if (opt == '?') error_code = ERROR_INPUT;
     markFlags(flags, opt);
   }
-  return error_code == SUCCESS ? optind : error_code;
+
+  return error_code;
 }
+
 void markFlags(Flags* flags, int opt) {
   switch (opt) {
     case 'b':
@@ -79,66 +75,93 @@ void markFlags(Flags* flags, int opt) {
       break;
   }
 }
-void catFile(FILE* file, Flags* flags) {
-  int isPrevBlank = 0;          // for -s
-  int ch, prevCh, counter = 1;  // counter  for -n or -b
-  for (prevCh = '\n'; (ch = fgetc(file)) != EOF; prevCh = ch) {
-    // if prevCh is \n is because we are at the beggining of the new line
-    if (prevCh == '\n') {
-      // for cases where there are a lot of linea breaks
-      if (flags->s) {
-        if (ch == '\n') {
-          if (isPrevBlank) continue;
-          isPrevBlank = 1;
-        } else
-          isPrevBlank = 0;
-      }
-      // line counting
-      if (flags->b) {
-        if (ch != '\n')
-          printf("%6d\t", counter++);
-        else if (flags->e)
-          printf("%6s\t", "");  // reserve a space 6 spaces + tab for $
-      } else if (flags->n)
-        printf("%6d\t", counter++);
-    }
-    // for -e, -t and -v cases
-    if (ch == '\n') {
-      if (flags->e) putchar('$');
-    } else if (ch == '\t') {
-      if (flags->t) {
-        fputs("^I", stdout);
-        continue;
-      }
-    } else if (!isprint(ch)) {
-      if (flags->v) {
-        printNonPrinting(ch);
-        continue;
-      }
-    }
-    putchar(ch);  // final print
+
+void catFile(const char *filename, Flags flags, int *line_num) {
+  FILE *f = filename ? fopen(filename, "r") : stdin;
+  if (!f) {
+    perror(filename);
+    return;
   }
+
+  char *line = NULL;
+  size_t bufn = 0;
+  ssize_t len;
+  int prev_blank = 0;
+
+  while ((len = getline(&line, &bufn, f)) != -1) {
+    int is_blank = (len == 1 && line[0] == '\n');
+    int skip = (flags.s && is_blank && prev_blank);
+
+    if (!skip) {
+      if (flags.b) {
+        if (!is_blank) {
+          printf("%6d\t", (*line_num)++);
+        }
+      } else if (flags.n) {
+        printf("%6d\t", (*line_num)++);
+      }
+
+      if (is_blank) {
+        if (flags.e) putchar('$');
+        putchar('\n');
+      } else {
+        for (ssize_t i = 0; i < len; ++i) {
+          unsigned char c = (unsigned char)line[i];
+
+          if (c == '\n') {
+            if (flags.e) putchar('$');
+            putchar('\n');
+          } else if (c == '\t') {
+            if (flags.t)
+              fputs("^I", stdout);
+            else
+              putchar('\t');
+          } else if (flags.v) {
+            handleNonPrintable(c);
+          } else {
+            putchar(c);
+          }
+        }
+      }
+    }
+
+    prev_blank = is_blank;
+  }
+
+  free(line);
+  if (f != stdin) fclose(f);
 }
 
-void printNonPrinting(int c) {
-  if (c & ~0x7F) {
-    fputs("M-", stdout);
-    c &= 0x7F;
-  }
-  if (iscntrl(c)) {
-    c = (c == '\177') ? '?' : (c | 0100);
+
+
+void handleNonPrintable(unsigned char c) {
+  if (c >= 128) {
+    unsigned char ch = c & 0x7F;
+    if (ch < 32) {
+      fputs("M-^", stdout);
+      putchar(ch + 64);
+    } else if (ch == 127) {
+      fputs("M-^?", stdout);
+    } else {
+      fputs("M-", stdout);
+      putchar(ch);
+    }
+  } else if (c < 32) {
     putchar('^');
+    putchar(c + 64);
+  } else if (c == 127) {
+    fputs("^?", stdout);
+  } else {
+    putchar(c);
   }
-  putchar(c);
 }
 
-void usage() {
-  printf(
-      "usage: [-benstv] [file...]\n"
-      "  -b  number nonempty lines\n"
-      "  -e  show $ at end of each line\n"
-      "  -n  number all lines\n"
-      "  -s  squeeze blank lines\n"
-      "  -t  show tabs as ^I\n"
-      "  -v  show nonprinting as ^X\n");
+
+void printUsage(const char *programName) {
+  fprintf(stderr,
+          "Usage: %s [ -b | --number-nonblank ] [ -n | --number ] "
+          "[ -s | --squeeze-blank ] [ -e ] [ -E | --show-ends ] "
+          "[ -t ] [ -T | --show-tabs ] [ -v | --show-nonprinting ] "
+          "[files...]\n", programName);
 }
+
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,21 +1,21 @@
 #ifndef _S21_CAT_H_
 #define _S21_CAT_H_
 
+#include <getopt.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define SUCCESS 0
 #define ERROR_INPUT -1
 #define ERROR_FILE_READ -2
 
-#include <stdio.h>
-
 typedef struct {
-    int b, e, n, s, t, v;
+  int b, e, n, s, t, v;
 } Flags;
 
-
+void catFile(const char *filename, Flags flags, int *line_num);
 int parseInput(int argc, char* argv[], Flags* flags);
 void markFlags(Flags* flags, int opt);
-void catFile(FILE* file, Flags* flags);
-void printNonPrinting(int c);
-void usage();
+void handleNonPrintable(unsigned char c);
+void printUsage(const char *programName);
 #endif
